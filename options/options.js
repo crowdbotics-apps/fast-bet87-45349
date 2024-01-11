0d1486471c8f0c00ace7fc3ef6b02b43dd08001b
Crowdbotics <team@crowdbotics.com>
2024-01-11 01:13:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fast-bet87-45349",
+  url: "https://fast-bet87-45349.botics.co",
+  api: "https://fast-bet87-45349.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
